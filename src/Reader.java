4cbb0d77df9a861843daff99c97adea56eca9428
franRei <38581197+franRei@users.noreply.github.com>
2020-05-23 12:20:55 +0200

    Update Reader.java

--- src/Reader.java
+++ src/Reader.java
@@ -4,19 +4,8 @@ import java.util.ArrayList;
 public class Reader {
 
     public static void main(String[] args) throws IOException {
-        //ArrayList<String> data = new ArrayList<>();
-        //Database_pgermanic database_pg = new Database_pgermanic();
-        //Database_wiki database_wiki = new Database_wiki();
-
-        //data = database_pg.open_Database();
-        //printIt.write_file(data);
-
-        //eigentliches Programm
-        //Test.testRun();
+        
         Test.testAll();
-        //Test.testWriter();
-        //Test.testGetLanguage();
-//        Read_XML reader = new Read_XML();
-  //      reader.parseFile(file, toFind);
+    
     }
 }
